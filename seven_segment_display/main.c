aab3aa3a92fc3f18e6baa58fc9d9de69ac8f21a9
Nicholas Morris <PrinceMorris@nicholas-morriss-imac.local>
2018-12-18 11:09:49 +1000

    Initial Commit

---
+++ seven_segment_display/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  seven_segment_display
+//
+//  Created by Nicholas Morris on 18/12/18.
+//  Copyright © 2018 Nicholas Morris. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
